6777804fb9abb0ae84895a623e274600ce6cc6ff
Richard Hsieh <richardhsieh1@hotmail.com>
2017-03-01 01:07:21 +0000

    Migration with Knex

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+const settings = require("./settings");
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user: settings.user,
+      password: settings.password,
+      filename: './dev.pg'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
